069fb2bad21ae56f4b795f17580856f204e023f1
komazirou <takahumikobayashi12192214@gmail.com>
2025-04-16 07:10:17 +0900

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <form onSubmit={props.getMealData}>
+      <input
+        onChange={(e) => setMealName(e.target.value)}
+        type="text"
+        name="mealName"
+        placeholder="料理名を英語で入力"
+      />
+      <button>検索</button>
+    </form>
+  );
+};
+export default Form;
